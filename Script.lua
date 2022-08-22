1b7980bc51fc582ef219322ab6cba09388a8a5c3
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-08-22 03:02:10 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,9 @@
 if game.PlaceId == 8750997647 then
   
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/TLX.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/Games/TLX.lua"))()
+elseif game.PlaceId == 9285238704 then
+  
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/Games/Race-Clicker.lua"))()
 else
   
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/Games.lua"))()
