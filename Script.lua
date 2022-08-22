d47495456c3c90350435620ee9bc84ea173cd94a
Streamy <107280696+StreamyShadow@users.noreply.github.com>
2022-08-22 16:33:08 -0600

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -8,6 +8,9 @@ elseif game.PlaceId == 10325366821 then
   
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/Games/Oof-Tycoon.lua"))()
 else
-  
+
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/Games/Anime-Hero-Sim.lua"))()
+else
+
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/Games.lua"))()
 end
