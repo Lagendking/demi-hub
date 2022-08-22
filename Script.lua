77c27541a8f19af12252c7c282d60fa7887b4410
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-08-22 01:30:22 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,6 @@
 if game.PlaceId == 8750997647 then
   
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/TLX.lua"))()
 else
   
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lagendking/demi-hub/main/Games.lua"))()
