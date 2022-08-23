9a75788c50af08386df6eaf1041784b940c12dc4
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-08-23 12:38:26 -0400

    Update Rainbow-Friends.lua

--- Games/Rainbow-Friends.lua
+++ Games/Rainbow-Friends.lua
@@ -23,6 +23,44 @@ local Tab3 = Window:MakeTab({
 
 Tab3:AddParagraph("                                             Credits", string.format("\nRainbow Friends Script: Lagend#4320 \nDemi Hub: Streamy#1480, Lagend#4320"))
 
+Tab:AddButton({
+	Name = "Get Items",
+	Callback = function(Value)
+	    
+		spawn(function()
+	        wait(.5)
+		        
+	        for _, v in pairs(workspace:GetDescendants()) do
+	            
+	            if v.Name:sub(0, -2) == "Block" or v.Name:sub(0, -3) == "Block" or v.Name == "FoodPink" or v.Name == "FoodGreen" or v.Name == "FoodOrange" or v.Name:sub(0, -2) == "Fuse" or v.Name:sub(0, -3) == "Fuse" or v.Name == "Battery" then
+	                
+	                if v:FindFirstChild("TouchTrigger") then
+	                    
+		                v.TouchTrigger.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+		                
+	                elseif v:IsA("Tool") and not game.Players.LocalPlayer.Character:FindFirstChild(v.Name) then
+	                    
+	                    v:Clone().Parent = game.Players.LocalPlayer.Character
+		            end
+		        end
+	        end
+		end)
+    end
+})
+
+
+Tab:AddButton({
+	Name = "Theater tp",
+	Callback = function(Value)
+	    
+		spawn(function()
+		        
+            game.Players.LocalPlayer.Character:MoveTo(game:GetService("Workspace")["Map_C1"].BlockTowerSpawn.Position)
+		end)
+    end
+})
+
+
 Tab:AddSection({
     Name = "esp"
 })
@@ -100,42 +138,4 @@ Tab:AddToggle({
             end
         end)
 	end
-})
-
-
-Tab:AddButton({
-	Name = "Get Items",
-	Callback = function(Value)
-	    
-		spawn(function()
-	        wait(.5)
-		        
-	        for _, v in pairs(workspace:GetDescendants()) do
-	            
-	            if v.Name:sub(0, -2) == "Block" or v.Name:sub(0, -3) == "Block" or v.Name == "FoodPink" or v.Name == "FoodGreen" or v.Name == "FoodOrange" or v.Name:sub(0, -2) == "Fuse" or v.Name:sub(0, -3) == "Fuse" or v.Name == "Battery" then
-	                
-	                if v:FindFirstChild("TouchTrigger") then
-	                    
-		                v.TouchTrigger.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-		                
-	                elseif v:IsA("Tool") and not game.Players.LocalPlayer.Character:FindFirstChild(v.Name) then
-	                    
-	                    v:Clone().Parent = game.Players.LocalPlayer.Character
-		            end
-		        end
-	        end
-		end)
-    end
-})
-
-
-Tab:AddButton({
-	Name = "Theater tp",
-	Callback = function(Value)
-	    
-		spawn(function()
-		        
-            game.Players.LocalPlayer.Character:MoveTo(game:GetService("Workspace")["Map_C1"].BlockTowerSpawn.Position)
-		end)
-    end
 })
